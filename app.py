7d090cec64a5916244531967b3c4f57ab4aec5f6
kumarsaikat321 <42418256+kumarsaikat321@users.noreply.github.com>
2026-02-15 23:42:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import os
 from sklearn.metrics import (accuracy_score, roc_auc_score, precision_score, 
                              recall_score, f1_score, matthews_corrcoef,
                              confusion_matrix, classification_report)
